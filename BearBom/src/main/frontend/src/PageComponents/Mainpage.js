7d70faa71f4a46f4fcf397755f4dbc6b75c37894
jhyunlee94 <jkwhd12@gmail.com>
2022-08-18 17:48:27 +0900

    0818

--- BearBom/src/main/frontend/src/PageComponents/Mainpage.js
+++ BearBom/src/main/frontend/src/PageComponents/Mainpage.js
@@ -3,16 +3,33 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "../css/mainpage.css";
+import { Button, Checkbox } from "@mui/material";
+import { pink } from "@mui/material/colors";
+
 const Mainpage = () => {
   return (
     <>
-      <Container>
-        <Row>
-          <Col style={{ textAlign: "center" }}>1 of 3</Col>
-          <Col style={{ textAlign: "center" }}>1 of 3</Col>
-          <Col style={{ textAlign: "center" }}>1 of 3</Col>
-        </Row>
-      </Container>
+      <Checkbox defaultChecked color="default" />
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+      <Checkbox
+        defaultChecked
+        sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
+      />
+      <Checkbox
+        defaultChecked
+        sx={{
+          color: pink[800],
+          "&.Mui-checked": {
+            color: pink[600],
+          },
+        }}
+      />
     </>
   );
 };
