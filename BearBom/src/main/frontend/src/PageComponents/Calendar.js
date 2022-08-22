b2be678d76b028454b580afdb301b538aff95e94
Debonchocola <pub10@naver.com>
2022-08-22 11:24:34 +0900

    test
    test

--- BearBom/src/main/frontend/src/PageComponents/Calendar.js
+++ BearBom/src/main/frontend/src/PageComponents/Calendar.js
@@ -1,18 +1,37 @@
 import React, { useState } from "react";
-import Calendar from "react-calendar";
 import "../css/calendar.css";
-import moment from "moment";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-function MyApp() {
-  const [value, onChange] = useState(new Date());
+function Calendar() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   return (
-    <>
-      <Calendar onChange={onChange} value={value} />
-      <div className="text-gray-500 mt-4">
-        {moment(value).format("YYYY년 MM월 DD일")}
+    <section>
+      <sectiontitle>(필수) 프로젝트 기간</sectiontitle>
+      <sectionInfo>프로젝트의 진행 기간을 선택해주세요.</sectionInfo>
+      <div>
+        <projectDateTitle>프로젝트 시작일: </projectDateTitle>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        />
       </div>
-    </>
+      <div>
+        <projectDateTitle>프로젝트 종료일:</projectDateTitle>
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          endDate={endDate}
+          minDate={startDate}
+        />
+      </div>
+    </section>
   );
 }
 
